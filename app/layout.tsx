1c9a7b10e30a1e7de168778d42900ca2e242255a
Akasho09 <aakashbwd@gmail.com>
2025-04-18 18:27:20 +0530

    openGraphAdded

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,63 @@
+import type { Metadata, Viewport } from "next";
 import Layout from "@/components/layyy";
 import PlanetOrbit from "@/components/mainpage/bg";
 import "./globals.css";
 import NavBar from "@/components/navs";
 import { Arrow } from "@/components/navs";
 import Me from "@/components/mySelf";
+
+export const metadata: Metadata = {
+  title: {
+    template: "Akash %s",
+    default: "Akash Ahmad Malik - Portfolio",
+  },
+  description: "Hi There! Let's build cool things together. You in? 👾",
+  keywords: [
+    "Akash Ahmad Malik",
+    "vin kya karas be cxei wan",
+    "software developer",
+    "portfolio",
+    "web development",
+    "Next.js",
+    "React",
+    "technology",
+    "projects",
+  ],
+  authors: [{ name: "Akash Ahmad Malik" }],
+  robots: "index, follow",
+  icons: {
+    icon: "/image.png",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Akash Ahmad Malik's Portfolio",
+    description:
+      "Check out Akash Ahmad Malik's portfolio for software projects and tech skills.",
+    creator: "@Aakash41339611",
+    images: ["akash.jpeg"],
+  },
+  openGraph: {
+    title: "Akash Ahmad Malik - Portfolio",
+    url: "https://akash09.vercel.app",
+    siteName: "Akash Ahmad Malik",
+    images: [
+      {
+        url: "akash.jpeg",
+        width: 600,
+        height: 600,
+        alt: "Akash Ahmad Malik Portfolio Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  height: "device-height",
+  initialScale: 1,
+};
 
 export default function RootLayout({
   children,
