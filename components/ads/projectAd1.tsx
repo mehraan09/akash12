5483a39db793b625d850a23478a148a9f2b9e8be
Akash Ahmad  Malik <aakashbwd@gmail.com>
2026-05-12 01:33:14 +0530

    project ad1

--- components/ads/projectAd1.tsx
+++ components/ads/projectAd1.tsx
@@ -4,30 +4,29 @@ import { useEffect } from "react";
 
 declare global {
   interface Window {
-    adsbygoogle: { push: (args: object) => void };
+    adsbygoogle: unknown[];
   }
 }
 
-export default function AdsenseAd() {
+export default function AdUnit() {
   useEffect(() => {
     try {
-      window.adsbygoogle = window.adsbygoogle || [];
-      window.adsbygoogle.push({});
+      if (typeof window !== "undefined") {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+      }
     } catch (err) {
-      console.error("AdSense error:", err);
+      console.error(err);
     }
   }, []);
 
   return (
     <ins
       className="adsbygoogle"
-      style={{
-        display: "inline-block",
-        width: "240px",
-        height: "720px",
-      }}
+      style={{ display: "block" }}
       data-ad-client="ca-pub-6934180729383134"
-      data-ad-slot="1750568111"
+      data-ad-slot="3210465734"
+      data-ad-format="auto"
+      data-full-width-responsive="true"
     />
   );
 }
